107fba3f0ad1ceb002a609d82902214bb42c06db
danpolus <danpol@gmail.com>
2022-04-06 19:42:35 +0300

    support skip fit
    full_gab: remove unnesessary code, fix bugs for skip fit
    
    change spatial models names

--- +bt/+model/full_gab_spatial.m
+++ +bt/+model/full_gab_spatial.m
@@ -1,4 +1,4 @@
-classdef spatial2d_gab < bt.model.template_spatial
+classdef full_gab_spatial < bt.model.template_spatial
 	% This is a template function that allows rapid prototyping of new spatial
 	% fitting by leveraging model.params.spatial_spectrum with absolutely
 	% no regard for performance
@@ -9,38 +9,22 @@ classdef spatial2d_gab < bt.model.template_spatial
 	end
 
 	methods
-		function self = spatial2d_gab() % Constructor
-			self.name = 'spatial2d_gab';
+		function self = full_gab_spatial() % Constructor
+			self.name = 'full_gab_spatial';
 			
             self.param_names = {'Gee','Gei','Ges','Gse','Gsr','Gsn','Gre','Grs','Alpha','Beta','t0','EMGa',...
-                                  'Gee_x_var','Gee_y_var','Gee_x_ph','Gee_y_ph',...
-                                  'Gei_x_var','Gei_y_var','Gei_x_ph','Gei_y_ph',...
-                                  'Gsn_x_var','Gsn_y_var','Gsn_x_ph','Gsn_y_ph',...
-                                  'Alpha_x_var','Alpha_y_var','Alpha_x_ph','Alpha_y_ph',...
-                                  'Beta_x_var','Beta_y_var','Beta_x_ph','Beta_y_ph',...
-                                  't0_x_var','t0_y_var','t0_x_ph','t0_y_ph'};
-            self.param_symbols = {'G_{ee}','G_{ei}','G_{es}','G_{se}','G_{sr}','G_{sn}','G_{re}','G_{rs}','\alpha','\beta','t_0','A_{EMG}',...
-                                 'Gee_{x_var}','Gee_{y_var}','Gee_{x_ph}','Gee_{y_ph}',...
-                                 'Gei_{x_var}','Gei_{y_var}','Gei_{x_ph}','Gei_{y_ph}',...
-                                 'Gsn_{x_var}','Gsn_{y_var}','Gsn_{x_ph}','Gsn_{y_ph}',...
-                                 'Alpha_{x_var}','Alpha_{y_var}','Alpha_{x_ph}','Alpha_{y_ph}',...
-                                 'Beta_{x_var}','Beta_{y_var}','Beta_{x_ph}','Beta_{y_ph}',...
-                                 't0_{x_var}','t0_{y_var}','t0_{x_ph}','t0_{y_ph}'}; 
+                                'Gee_amp','Gei_amp','Gsn_amp','t0_amp','Alpha_amp','Beta_amp'};                              
+			self.param_symbols = {'G_{ee}','G_{ei}','G_{es}','G_{se}','G_{sr}','G_{sn}','G_{re}','G_{rs}','\alpha','\beta','t_0','A_{EMG}',...
+                                'Gee_{amp}','Gei_{amp}','Gsn_{amp}','t_{0amp}','Alpha_{amp}','Beta_{amp}'};                             
 			self.param_units = {'','','','','','','','','s^{-1}','s^{-1}','ms','',...
-                                '','','m','m',...
-                                '','','m','m',...
-                                '','','m','m',...
-                                's^{-1}','s^{-1}','m','m',...
-                                's^{-1}','s^{-1}','m','m',...
-                                'ms','ms','m','m'};                             
+                                '','','','ms','s^{-1}','s^{-1}'};                            
             self.initial_step_size = [0.4    0.4     0.5     0.5    0.4    0.4    0.1     0.3      5     40    0.005  0.05];
 		   	self.limits =            [eps    -20     eps     eps    -20    eps    eps     eps     10    100    0.075     0 ;...
                                       20    -eps      20      20   -eps    40     10      20    100    800     0.14     1 ];
-            p = model.params();
-            self.initial_step_size = [self.initial_step_size  0.1  0.1  0.05  0.05   0.1  0.1  0.05  0.05   0.2  0.2  0.05  0.05   0.5  0.5  0.05  0.05      3    3  0.05  0.05    0.001  0.001  0.05  0.05]; 
-            self.limits =            [self.limits             [-4   -4     0     0    -4   -4     0     0   -10  -10     0     0   -40  -40     0     0   -300 -300     0     0    -0.03  -0.03     0     0;...
-                                                                4    4  p.Lx  p.Ly     4    4  p.Lx  p.Ly    10   10  p.Lx  p.Ly    40   40  p.Lx  p.Ly    300  300  p.Lx  p.Ly     0.03   0.03  p.Lx  p.Ly]];             
-                                                            
+            self.initial_step_size = [self.initial_step_size  0.1  0.1  0.2  0.001 0.5  3]; 
+            self.limits =            [self.limits             [-4   -4  -10  -0.03 -40 -300;...
+                                                                4    4   10   0.03  40  300]];                                                                    
+                                                           
             self.n_params = length(self.param_names);                                
 			self.n_fitted = self.n_params;
 			self.skip_fit = zeros(1,self.n_fitted);    
@@ -66,16 +50,16 @@ classdef spatial2d_gab < bt.model.template_spatial
 			% This function is what decides what the spatial variations are actually going to be
 			%e.g. p.apply_variation('cosine','t0',pars(10));
             self.p = p_from_fitted_params(self,self.p,pars);
-            self.p.apply_variation('g_ee','costume_doc_2d_phased',pars(13),pars(14),pars(15),pars(16));
-            self.p.apply_variation('g_ei','costume_doc_2d_phased',pars(17),pars(18),pars(19),pars(20));
-            self.p.apply_variation('g_sn','costume_doc_2d_phased',pars(21),pars(22),pars(23),pars(24));
-            self.p.apply_variation('alpha','costume_doc_2d_phased',pars(25),pars(26),pars(27),pars(28));
-            self.p.apply_variation('beta','costume_doc_2d_phased',pars(29),pars(30),pars(31),pars(32));
-            self.p.apply_variation('t0','costume_doc_2d_phased',pars(33),pars(34),pars(35),pars(36));
+            self.p.apply_variation('g_ee','costume_doc',pars(13));
+            self.p.apply_variation('g_ei','costume_doc',pars(14));
+            self.p.apply_variation('g_sn','costume_doc',pars(15));
+            self.p.apply_variation('t0','costume_doc',pars(16));
+            self.p.apply_variation('alpha','costume_doc',pars(17));
+            self.p.apply_variation('beta','costume_doc',pars(18));
         end
 		
 		function [initial_values,prior_pp] = initialize_fit(self,target_f,target_P) % Return the first posterior/prior and initial values
-            initial_vals_variation = zeros(1,6*4);
+            initial_vals_variation = [0 0 0 0 0 0];
 			[f1,P1,idx,db_data,min_chisq] = db_fit.quick_fit(self,target_f,mean(target_P,2)); % Fit the first (or only) spectrum given
 			p = model.params(db_data.iswake(idx));
             initial_values = [db_data.gab(idx,:) p.alpha(1) p.beta(1) p.t0 eps initial_vals_variation];
